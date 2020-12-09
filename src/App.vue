c24d06c5059565bb08f29122c393026e655f29ee
frlzjosh <joshua.magdaleno.472@my.csun.edu>
2020-12-09 01:56:05 -0800

    initializing repo

--- src/App.vue
+++ src/App.vue
@@ -1,13 +1,36 @@
 <template>
   <div id="app">
     <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
+      <router-link to="/">Temperature</router-link> |
+      <router-link to="/statistics">Statistics</router-link>
     </div>
     <router-view />
   </div>
 </template>
+<script>
+import Vue from "vue";
+import { createNamespacedHelpers } from "vuex";
+const { mapActions, mapGetters } = createNamespacedHelpers("temp");
+export default Vue.extend({
+  name: 'App',
+  sockets: {
+    connect: () => {
+      console.log("websocket connected!");
+    },
+    temperatureData: function(data) {
+      this.updateTemperatureData(data);
+    },
+    newStream: function(data) {
+      console.log('data: ', data)
+      this.initTemperatureData(data);
+    }
+  },
+  methods: {
+      ...mapActions(["updateTemperatureData", "initTemperatureData"]),
 
+  }
+})
+</script>
 <style lang="scss">
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
--- src/store/index.ts
+++ src/store/index.ts
@@ -1,11 +1,20 @@
 import Vue from "vue";
-import Vuex from "vuex";
+import Vuex, { StoreOptions} from 'vuex';
+import {temp} from "./modules/temp/tempStore";
 
 Vue.use(Vuex);
 
-export default new Vuex.Store({
-  state: {},
-  mutations: {},
-  actions: {},
-  modules: {}
-});
+export interface RootState{
+  version: string;
+}
+
+const store: StoreOptions<RootState> = {
+  state: {
+    version: '1.0.0'
+  },
+  modules: {
+    temp
+  }
+}
+
+export default new Vuex.Store<RootState>(store);
